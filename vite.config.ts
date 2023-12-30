a148313a7751dc0444471c8230d72357fae1b0c9
Vladislav Lipatov <lipatov.xex@gmail.com>
2023-12-30 10:53:35 +0700

    Added build config and package.json info

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    dts({
+      entryRoot: "src/lib",
+    }),
+    solid(),
+  ],
+  build: {
+    lib: {
+      entry: "src/lib/index.tsx",
+      name: "solid-simple-popover",
+      fileName: "index",
+      formats: ["es"],
+    },
+    emptyOutDir: true,
+    outDir: "dist",
+    minify: false,
+    rollupOptions: {
+      external: ["solid-js", "solid-js/web", "@floating-ui/dom"],
+    },
+  },
+});
